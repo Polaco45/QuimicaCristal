dfa26ee8b622beddc644b4419d2f4890eb33b447
Polaco45 <94990156+Polaco45@users.noreply.github.com>
2025-05-09 13:20:30 -0300

    Add files via upload

---
+++ pos_fiscal_position_extension_v6/pos_fiscal_position_extension/static/src/js/fiscal_position_hook.js
@@ -0,0 +1,23 @@
+odoo.define('pos_fiscal_position_extension.fiscal_position_hook', function (require) {
+    "use strict";
+
+    const models = require('point_of_sale.models');
+    const _super_order = models.Order.prototype;
+
+    models.load_fields('res.partner', ['fiscal_position_id']);
+
+    models.Order = models.Order.extend({
+        set_client: function (client) {
+            _super_order.set_client.apply(this, arguments);
+            if (client && client.fiscal_position_id) {
+                this.fiscal_position_id = client.fiscal_position_id[0];
+            }
+        },
+        export_as_JSON: function () {
+            const json = _super_order.export_as_JSON.apply(this, arguments);
+            json.fiscal_position_id = this.fiscal_position_id;
+            json.selected_journal_id = this.selected_journal_id || false;
+            return json;
+        }
+    });
+});
