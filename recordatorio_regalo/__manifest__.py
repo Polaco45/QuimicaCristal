2e3b6746da7b0404cb76ec54629ee25b01b406fb
Polaco45 <94990156+Polaco45@users.noreply.github.com>
2025-05-21 12:28:50 -0300

    Delete recordatorio_regalo directory

--- recordatorio_regalo/__manifest__.py
+++
@@ -1,9 +0,0 @@
-{
-    'name': 'Recordatorio Regalo No Usado',
-    'version': '1.0',
-    'category': 'Marketing',
-    'depends': ['sale', 'base', 'whatsapp_integration'],  # adaptalo según tus dependencias reales
-    'data': [],
-    'installable': True,
-    'application': False,
-}
--- recordatorio_regalo/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import partner_regalo_reminder
