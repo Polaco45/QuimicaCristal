f2c99abc0ded00f428c0035a7c1b742b07108e86
Polaco45 <94990156+Polaco45@users.noreply.github.com>
2025-05-21 12:21:00 -0300

    Create __manifest__.py

---
+++ recordatorio_regalo/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Recordatorio Regalo No Usado',
+    'version': '1.0',
+    'category': 'Marketing',
+    'depends': ['sale', 'base', 'whatsapp_integration'],  # adaptalo según tus dependencias reales
+    'data': [],
+    'installable': True,
+    'application': False,
+}
