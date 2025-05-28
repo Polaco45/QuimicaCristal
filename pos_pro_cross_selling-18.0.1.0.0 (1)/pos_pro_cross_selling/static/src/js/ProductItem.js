405b116b0a82e309d273b00d680e45e364b97d98
Polaco45 <94990156+Polaco45@users.noreply.github.com>
2025-05-28 11:35:49 -0300

    Update ProductItem.js

--- pos_pro_cross_selling-18.0.1.0.0 (1)/pos_pro_cross_selling/static/src/js/ProductItem.js
+++ pos_pro_cross_selling-18.0.1.0.0 (1)/pos_pro_cross_selling/static/src/js/ProductItem.js
@@ -1,24 +1,27 @@
 /** @odoo-module **/
+
 import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product_screen";
-import { rpc } from "@web/core/network/rpc";;
+import { rpc } from "@web/core/network/rpc";
 import { patch } from "@web/core/utils/patch";
 import { CrossProduct } from "@pos_pro_cross_selling/app/cross_product/cross_product";
-import { reactive } from "@odoo/owl";
 
-//Patching ProductScreen
 patch(ProductScreen.prototype, {
-//    function to add the selected product to the order
     async addProductToOrder(product, options = {}) {
-        var ProductSelected = await reactive(this.pos).addLineToCurrentOrder({ product_id: product }, {});
+        // Agregamos el producto normalmente al pedido
+        const order = this.pos.get_order();
+        const productObj = this.pos.db.get_product_by_id(product.id);
+        order.add_product(productObj);
+
+        // Pedimos productos relacionados al backend
         rpc('/web/dataset/call_kw/pos.cross.selling/get_cross_selling_products', {
             model: 'pos.cross.selling',
             method: 'get_cross_selling_products',
-            args: [[],ProductSelected?._raw.product_id.id],
+            args: [[], product.id],
             kwargs: {},
         }).then(async(result) => {
             if (result.length > 0) {
                 await this.dialog.add(CrossProduct, {
-                   product: result
+                    product: result
                 });
             }
         });
