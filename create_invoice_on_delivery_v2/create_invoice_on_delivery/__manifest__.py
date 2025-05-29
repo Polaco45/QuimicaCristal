be7f311fff6b8512b933b31cd08f6ff77655ea40
Polaco45 <94990156+Polaco45@users.noreply.github.com>
2025-05-29 10:36:31 -0300

    Update __manifest__.py

--- create_invoice_on_delivery_v2/create_invoice_on_delivery/__manifest__.py
+++ create_invoice_on_delivery_v2/create_invoice_on_delivery/__manifest__.py
@@ -1,8 +1,9 @@
 {
     "name": "Create Invoice on Delivery",
     "version": "1.0",
+    "license": "LGPL-3",
     "depends": ["sale", "stock", "account"],
-    "author": "Polaco & ChatGPT",
+    "author": "Polaco45 & ChatGPT",
     "category": "Automation",
     "description": "Crea y valida automáticamente una factura al validar una entrega (stock.picking).",
     "data": [],
