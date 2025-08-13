6dcaadac87fcb9ff1acd59e171d8578d6ea29911
Joacoramello <joaquinramello@gmail.com>
2025-08-13 13:39:45 -0300

    Delete custom-addons/sale_affiliate_autopps_clean directory

--- custom-addons/sale_affiliate_autopps_clean/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
-
-    x_affiliate_key = fields.Char(string='Affiliate Key', index=True, copy=False)
