65f222471d44b05dc10b27af60c7f117aa88e3b2
Polaco45 <94990156+Polaco45@users.noreply.github.com>
2025-09-19 10:13:58 -0300

    Update __manifest__.py

--- custom-addons/affiliate_customer_ext/__manifest__.py
+++ custom-addons/affiliate_customer_ext/__manifest__.py
@@ -1,18 +1,14 @@
-# __manifest__.py (v18)
+# __manifest__.py
 {
     "name": "Affiliate Customer Extension",
     "version": "18.0.1.0.0",
     "summary": "Muestra el cliente en visitas de afiliado (backend y portal)",
-    'depends': ['affiliate_management', 'website', 'portal', 'website_sale'],
-    'data': [
-    'views/affiliate_visit_views.xml',   # backend
-    'views/portal_templates.xml',        # QWeb portal/website
+    "depends": ["affiliate_management", "website", "portal", "website_sale"],
+    "data": [
+        "views/affiliate_visit_views.xml",   # backend
+        "views/portal_templates.xml",        # QWeb portal/website
     ],
-    "assets": {
-        "web.assets_frontend": [
-            "affiliate_customer_ext/views/portal_templates.xml",
-        ],
-    },
     "license": "LGPL-3",
     "installable": True,
+    "application": False,
 }
