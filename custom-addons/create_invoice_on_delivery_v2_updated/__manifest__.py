b6794f925c4bc00d64ca2828ab5f929fd01b4822
Joacoramello <joaquinramello@gmail.com>
2026-01-16 11:32:54 -0300

    dd

--- custom-addons/create_invoice_on_delivery_v2_updated/__manifest__.py
+++ custom-addons/create_invoice_on_delivery_v2_updated/__manifest__.py
@@ -1,11 +1,11 @@
 {
     "name": "Create Invoice on Delivery",
-    "version": "1.0",
+    "version": "1.1",
     "license": "LGPL-3",
     "depends": ["sale", "stock", "account"],
     "author": "Polaco45 & ChatGPT",
     "category": "Automation",
-    "description": "Crea y valida automáticamente una factura al validar una entrega (stock.picking).",
+    "description": "Crea y valida automáticamente facturas al validar entregas (stock.picking), soportando entregas parciales (múltiples facturas por SO).",
     "data": [],
     "installable": True,
     "application": False,
